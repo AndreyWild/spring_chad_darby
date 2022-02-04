6df2dd00e6537d87c01adf24f5f54f4cea521c40
AndreyWild <and1rei@mail.ru>
2022-02-04 17:38:43 +0300

    commit_video_59

--- spring-demo-annotations/src/main/java/com/wild/springannotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/wild/springannotations/TennisCoach.java
@@ -5,16 +5,32 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component //("thatSillyCoach")
-@Scope("prototype") // default type
+//@Scope("prototype") // default type
 public class TennisCoach implements Coach {
 
     @Autowired
     @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
+    // define a default method
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println(">> TennisCoach: inside of  doMyStartupStuff()");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">> TennisCoach: inside of  doMyCleanupStuff()");
     }
 
 /*    public TennisCoach(FortuneService theFortuneService) {
